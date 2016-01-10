d9abba28940b79ed8d2c7a0542b0d7ac22e2a3b2
Abhilash <abhilash7725@gmail.com>
2016-01-10 12:46:24 +0530

    Create AGGRCOW_binary_search.cc

---
+++ Refer/AGGRCOW_binary_search.cc
@@ -0,0 +1,46 @@
+// http://www.spoj.com/problems/AGGRCOW/
+/*
+	binary search [over possible answer space]
+	O(n * max(xi))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 1e5 + 7;
+int ar[maxn];
+int n, c;
+
+int solve() {
+	int l = 1;
+	int r = (int)1e9;
+	while (l < r) {
+		int mid = (l + r + 1) / 2;
+		int next = 0;
+		int done = c;
+		for (int i = 0; i < n; ++i) {
+			if (ar[i] >= next) {
+				--done;
+				next = ar[i] + mid;
+			}
+		}
+		if (done <= 0) l = mid;
+		else r = mid - 1;
+	}
+	return r;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t; cin >> t;
+	while (t--) {
+		cin >> n >> c;
+		for (int i = 0; i < n; ++i) cin >> ar[i];
+		sort(ar, ar + n);
+		cout << solve() << "\n";
+	}
+
+	return 0;
+}
