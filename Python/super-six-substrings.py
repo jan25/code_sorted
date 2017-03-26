a1756f2a21c6be5e9117c23b37d6ee66115d6d28
jan25 <abhilashgnan@gmail.com>
2017-03-26 12:59:38 +0800

    Python solution folder addition with one py file

---
+++ Python/super-six-substrings.py
@@ -0,0 +1,28 @@
+'''
+https://www.hackerrank.com/contests/hourrank-18/challenges/super-six-substrings/submissions/code/1301089841
+Level: Medium
+O(1) space
+O(N) time
+bottom up dp
+'''
+
+#!/bin/python3
+import sys
+s = input().strip()
+def toInt(c):
+	return ord(c) - ord('0')
+ans = 0
+memo = [0 for x in range(6)]
+for i in range(len(s)):
+	temp = [0 for x in range(6)]
+	if toInt(s[i]) > 0: temp[toInt(s[i]) % 6] = 1;
+	if 0 == toInt(s[i]):
+		ans += 1
+		for y in range(len(memo)):
+			temp[(y * 10) % 6] += memo[y]
+	else:
+		for y in range(len(memo)):
+			temp[((y * 10) + toInt(s[i])) % 6] += memo[y]
+	memo = temp
+	ans += memo[0];
+print (ans)
